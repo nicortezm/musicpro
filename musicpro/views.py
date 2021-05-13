2aedabaf813d7f83e3e0cec3210086dcf117dae5
nicortezm <nascorman@gmail.com>
2021-05-13 17:02:00 -0400

    Agregados productos al inicio

--- musicpro/views.py
+++ musicpro/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+from tienda.models import Product
 # Create your views here.
 def home(request):
-    # products = Product.objects.all().filter(is_available=True)
-    # context = {
-    #     'products': products,
-    # }
-    return render(request, 'home.html')
+    products = Product.objects.all().filter(is_available=True)
+    context = {
+        'products': products,
+    }
+    return render(request, 'home.html',context)
