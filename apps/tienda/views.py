d5a82ca4334520a5a6a488ca165e1e0300feffd7
nicortezm <nascorman@gmail.com>
2021-05-13 16:38:27 -0400

    Agregados productos db

--- apps/tienda/views.py
+++ apps/tienda/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
+from .models import Product
+from category.models import Category
 
 # Create your views here.
-
--- musicpro/views.py
+++ musicpro/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    # products = Product.objects.all().filter(is_available=True)
+    # context = {
+    #     'products': products,
+    # }
     return render(request, 'home.html')
