5c3d361e86494daeeeb41ca9924a492f3815dc96
nicortezm <nascorman@gmail.com>
2021-05-13 16:24:05 -0400

    Agregado modelo productos

--- apps/tienda/models.py
+++ apps/tienda/models.py
@@ -18,3 +18,29 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name
 
+class Product(models.Model):
+    product_name = models.CharField("nombre producto",max_length=200,unique=True)
+    slug = models.SlugField(max_length=200,unique=True)
+    description = models.TextField("descripcion",max_length=500,blank=True)
+    price = models.IntegerField("precio")
+    images = models.ImageField("imagenes",upload_to='photos/products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField("disponible",default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,verbose_name = 'Categoria')
+
+    # Agregar Foreingkey marca.
+    # Agregar serial.
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'producto'
+        verbose_name_plural = 'productos'
+
+    def get_url(self):
+        return reverse('product_detail',args=[self.category.slug,self.slug])
+
+    def __str__(self):
+        return self.product_name
+
+ 
---
+++ apps/tienda/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
+
+# Create your views here.
+
