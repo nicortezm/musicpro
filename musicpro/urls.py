a033bf36bfeb65361ed552f3a1fa4f6a6a1d5b65
nicortezm <nascorman@gmail.com>
2021-05-16 15:42:13 -0400

    agregado carrito y paginador

--- musicpro/urls.py
+++ musicpro/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('tienda/',include('tienda.urls')),
+    path('cart/',include('carts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Administración MusicPro"
