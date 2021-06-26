7041335d4011fb71b657861e5b977b6e8e8a1c39
nicortezm <nascorman@gmail.com>
2021-06-26 12:07:27 -0400

    intento1

--- musicpro/settings.py
+++ musicpro/settings.py
@@ -203,7 +203,7 @@ MESSAGE_TAGS = {
 # Configuración Correo SMTP
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 25
 EMAIL_HOST_USER = 'djangodwy@gmail.com'
 EMAIL_HOST_PASSWORD = 'N@ncybern4l'
 EMAIL_USE_TLS = True
