1d08e2cd30d9825de028b499e2a3d509ec4f708c
Nicolas Cortez <nascorman@gmail.com>
2021-09-30 09:19:11 -0300

    Update settings.py

--- musicpro/settings.py
+++ musicpro/settings.py
@@ -207,8 +207,8 @@ MESSAGE_TAGS = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'djangodwy@gmail.com'
-EMAIL_HOST_PASSWORD = 'N@ncybern4l'
+EMAIL_HOST_USER = '*************'
+EMAIL_HOST_PASSWORD = '*************'
 EMAIL_USE_TLS = True
 
 # Descuento 
